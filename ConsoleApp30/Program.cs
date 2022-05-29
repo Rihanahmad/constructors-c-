affa5e87e28a75de7e7878addc3d1bf26bf8c4c9
Tajdar <Tajdar@TAJDAR-PC>
2022-05-30 02:41:36 +0530

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        string carmodel;
+        int ceryear;
+        int carcompy;
+        public Program(string model,int year,int compy)
+        {
+            carmodel = model;
+            ceryear = year;
+            carcompy = compy;
+
+
+
+            
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program("bmw",1990,1947);
+            Console.WriteLine(obj.carmodel);
+            Console.WriteLine(obj.ceryear);
+            Console.WriteLine(obj.carcompy);
+     
+            Console.ReadLine();
+        }
+    }
+}
